d232b747492efac2f6bb9c8a24832030dfa0e526
Hunner <h.herrera.academics@gmail.com>
2020-01-27 13:52:37 -0800

    first push to orgin

--- challenges.js
+++ challenges.js
@@ -34,7 +34,11 @@ addOne(-5) //=> -4
 -----------------------------------------------------------------*/
 // Your solution for 01-addOne here:
 
-
+function addOne(n) {
+  // let sum = n + 1;
+  // return sum;
+  return n+1;
+}
 
 
 
@@ -57,7 +61,7 @@ addTwoNumbers('Hello', 5) //=> NaN
 -----------------------------------------------------------------*/
 // Your solution for 02-addTwoNumbers here:
 
-
+// function addTwoNumbers() {}
 
 
 
